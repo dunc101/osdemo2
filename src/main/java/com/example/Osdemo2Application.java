f04bb1613e077911ad498c74742bb7729e94ec1e
dunc101 <dunc101@gmail.com>
2016-09-14 11:33:26 -0500

    ARCENG-172: Forgot to add the ping endpoint.

--- src/main/java/com/example/Osdemo2Application.java
+++ src/main/java/com/example/Osdemo2Application.java
@@ -2,11 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Osdemo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Osdemo2Application.class, args);
+	}
+	
+	@RequestMapping
+	public String ping() {
+		return "pong";
 	}
 }
