32db0f137103314c4bd42f71dbfa505ed8e76b98
dunc101 <dunc101@gmail.com>
2016-09-14 11:25:44 -0500

    ARCENG-172: Initial import of openshift demo example.

---
+++ src/main/java/com/example/Osdemo2Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Osdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Osdemo2Application.class, args);
+	}
+}
